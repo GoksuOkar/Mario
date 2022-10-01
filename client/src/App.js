e4bfbf47c3e00ed15775cd328a925dd8492d164c
Goksu Okar <goksuokar@gmail.com>
2022-10-01 11:42:27 -1000

    login conditional rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,17 @@
-import logo from './logo.svg';
-import axios from 'axios';
+import React, { useState } from 'react';
 import './App.css';
+import LoginView from './Login/LoginView.js'
 
 const serverURL = 'http://localhost:3001'
 
 function App() {
-  axios.get(serverURL).then((res) => {console.log(res)});
+  //const divRef = useRef(null);
+  const [userId, setUserId] = useState(null);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!userId && <LoginView login={setUserId} userId={userId}/>}
+      {userId && <div>Hello {userId}</div>}
     </div>
   );
 }
