e245ca20d1e955e348b1f4cc6cd762a8dcf5ba94
Timothy Loo <TimothyLoo07@gmail.com>
2022-10-01 11:02:20 -1000

    created nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import logo from './logo.svg';
-// import axios from 'axios';
+import axios from 'axios';
 import './App.css';
 import Dropdown from '../src/components/Messages/Messages.js';
+import NavBar from './components/NavBar.js';
 
-// const serverURL = 'http://localhost:3001';
+const serverURL = 'http://localhost:3001';
 
 export default function App() {
-  // axios
-  //   .get(serverURL)
-  //   .then((res) => console.log(res))
-  //   .catch((err) => console.log(err));
+  axios
+    .get(serverURL)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
 
   return (
     <div className='App'>
+      <NavBar />
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
