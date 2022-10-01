224691a91488344bd56906e488cd4b2605c6897b
Timothy Loo <TimothyLoo07@gmail.com>
2022-10-01 11:20:55 -1000

    commented out axios request

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
+// import axios from 'axios';
 import Dropdown from '../src/components/Messages/Messages.js';
 import NavBar from './components/NavBar.js';
 
 const serverURL = 'http://localhost:3001';
 
 export default function App() {
-  axios
-    .get(serverURL)
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
+  // axios
+  //   .get(serverURL)
+  //   .then((res) => console.log(res))
+  //   .catch((err) => console.log(err));
 
   return (
     <div className='App'>
