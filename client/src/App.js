ec75ce7fd5ce1cccf355053808a5674e8f67b708
Ivan Luk <luk.ivan.mech@gmail.com>
2022-10-01 11:02:43 -0700

    Deleted excess files, set up initial demo for Socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
 // import axios from 'axios';
-import './App.css';
 import Dropdown from '../src/components/Messages/Messages.js';
 
 // const serverURL = 'http://localhost:3001';
@@ -13,19 +11,6 @@ export default function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
       <Dropdown />
     </div>
   );
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,11 @@
 import express, { Express } from 'express';
+
 import morgan from 'morgan';
 import cors from 'cors';
+import http from 'http';
+
 import { router } from './routes';
+import { Server } from 'socket.io';
 
 const app: Express = express();
 
@@ -10,6 +14,24 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 const port = 3001;
+
+const socket = http.createServer(app);
+
+const io = new Server(socket, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["Get", "POST"],
+  },
+})
+
+io.on('connection', (socket) => {
+
+  socket.on('send_message', (data) => {
+    socket.broadcast.emit('receive_message', () => {
+
+    })
+  })
+})
 
 app.use('/', router);
 
