537dea3bae980d51afccfb9adb7707f74868fa53
Thach Do <thachdo90@gmail.com>
2022-10-02 09:44:25 -0700

    added logo image for form and installed mantine dates

--- client/src/components/Dashboard/MakeGame.jsx
+++ client/src/components/Dashboard/MakeGame.jsx
@@ -1,9 +1,11 @@
 import './MakeGame.css';
+import StartGameImg from '../../assets/images/StartGameImg.png';
 
 const MakeGame = () => {
   return (
     <>
-      <div id='left-form-panel'> Make a game
+      <div id='left-form-panel'>
+        <img src={StartGameImg} alt='basketball img with text start game'></img>
       </div>
       <div id='right-form-panel'> Form
       </div>
