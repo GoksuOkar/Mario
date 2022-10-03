5bac1ec8ab4175a413ee989f66cf3dcfb0d10a7d
Thach Do <thachdo90@gmail.com>
2022-10-03 09:56:41 -0700

    resized and organized components in dashboard

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { SegmentedControl, Button } from '@mantine/core';
+import React, { useState } from 'react';
+import { SegmentedControl, Button, Grid } from '@mantine/core';
 import EventCards from './EventCards.jsx';
 import UpcomingGames from './UpcomingGames.jsx';
 import MakeGame from './MakeGame.jsx';
@@ -13,27 +13,33 @@ const Dashboard = () => {
       <div>
         Dashboard
       </div>
-      <UpcomingGames />
-      <p>Sort by:</p>
-      <SegmentedControl
-        data={[
-          {label: 'upcoming', value: 'upcoming'},
-          {label: 'distance', value: 'distance'},
-          {label: 'friends', value: 'friends'}
-        ]}
-        value={sortBy}
-        onChange={setSortBy}
-        />
-      <EventCards sortBy={sortBy} setSortBy={setSortBy}/>
-      {/* later: turn this into a basketball */}
-      {/* link this to open up modal form */}
-      <Button
-        radius='xl'
-        size='xl'
-        onClick={()=>setFormOpen(true)}>
-        Make Game
-      </Button>
-      {formOpen && <MakeGame setFormOpen={setFormOpen}/>}
+      <Grid>
+        <Grid.Col span={2}>
+          <UpcomingGames />
+          {/* later: turn this into a basketball */}
+          {/* link this to open up modal form */}
+          <Button
+            radius='xl'
+            size='xl'
+            onClick={()=>setFormOpen(true)}>
+            Make Game
+          </Button>
+          {formOpen && <MakeGame setFormOpen={setFormOpen}/>}
+        </Grid.Col>
+        <Grid.Col span={8}>
+          <p>Sort by:</p>
+          <SegmentedControl
+            data={[
+              {label: 'upcoming', value: 'upcoming'},
+              {label: 'distance', value: 'distance'},
+              {label: 'friends', value: 'friends'}
+            ]}
+            value={sortBy}
+            onChange={setSortBy}
+            />
+          <EventCards sortBy={sortBy} setSortBy={setSortBy}/>
+        </Grid.Col>
+      </Grid>
     </>
   )
 
