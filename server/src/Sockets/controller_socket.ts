71f8ccde2a7eaff8ba9b802ce8469c4f6f4047fa
Ivan Luk <luk.ivan.mech@gmail.com>
2022-10-03 10:31:59 -0700

    updated conversations CRUD

--- server/src/Sockets/controller_socket.ts
+++ server/src/Sockets/controller_socket.ts
@@ -1,10 +1,9 @@
-import * as database from './../db/BlueOceanSchema';
+import * as mongo from './../db/BlueOceanSchema';
 import * as I from '../Utilities/Interfaces/Schemas';
-import { io } from '../server';
 
-export async function getConversationsFor (username: string): Promise<any> {
+export async function getConversationIDsFor (username: string): Promise<any> {
   try {
-    const user = await database.User.find({username: username}).exec();
+    const user = await mongo.User.find({username: username}).exec();
 
     return user;
   } catch (err) {
@@ -12,14 +11,21 @@ export async function getConversationsFor (username: string): Promise<any> {
   }
 }
 
-export async function updateConversation(message: I.Message) {
-  // populate database ?
+export async function updateConversation (message: I.Message): Promise<void> {
   try {
-    database.Conversation.findByIdAndUpdate({_id: message.conversationId}, {
+    mongo.Conversation.findByIdAndUpdate({_id: message.conversationId}, {
       $push: {
         messages: message
       }
-    })
+    }).exec();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function getConversationsFrom (ids: String[]): Promise<any> {
+  try {
+    return mongo.Conversation.find({'_id': {$in: ids} });
   } catch (err) {
     console.error(err);
   }
