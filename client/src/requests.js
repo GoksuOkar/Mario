310f7a3331c1a07135e7ab56d34fda5188173724
Thach Do <thachdo90@gmail.com>
2022-10-04 11:51:48 -0700

    commented out routes for now on server side

---
+++ client/src/requests.js
@@ -0,0 +1,59 @@
+// later repace with deployed server IP
+const basePath = 'http://localhost:3001';
+const axios = require('axios');
+
+module.exports = {
+  // GAMES
+  getAllGames: (city, state, sort='upcoming') => {
+    return axios({
+      url: '/games',
+      method: 'get',
+      baseURL: basePath,
+      params: { city, state, sort }
+    })
+  },
+
+  getOneGame: (gameId) => {
+    return axios({
+      url: '/games',
+      method: 'get',
+      baseURL: basePath,
+      params: { gameId }
+    })
+  },
+
+  createGame: (body) => {
+    return axios({
+      url: '/games',
+      method: 'post',
+      baseURL: basePath,
+      data: body
+    })
+  },
+
+  // USERS
+  getUserInfo: (userId) => {
+    return axios({
+      url: '/users',
+      method: 'get',
+      baseURL: basePath,
+      params: { userId }
+    })
+  },
+  joinGame: (gameId) => {
+    return axios({
+      url: '/users',
+      method: 'put',
+      baseURL: basePath,
+      params: { gameId }
+    })
+  },
+  getFriendInfo: (friendId) => {
+    return axios({
+      url: '/friends',
+      method: 'get',
+      baseURL: basePath,
+      params: { friendId }
+    })
+  },
+}
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,8 +4,19 @@ import * as controllers from './controllers';
 
 
 export const router = Router();
-
+// AUTH
 router.get('/auth', controllers.auth)
 router.post('/register', controllers.register)
 router.get('/login', controllers.login)
 router.delete('/logout', controllers.logout);
+
+// // GAMES
+// router.get('./games', )
+// router.post('./games', )
+
+// // USERS
+// router.get('/users', )
+// router.get('/friends', )
+
+// // CONVERSATIONS
+
