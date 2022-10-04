25b0312dd01a213673ba7864716e3f69009039f2
Goksu Okar <goksuokar@gmail.com>
2022-10-04 13:32:27 -1000

    auth moved up to app to prevent double rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { LoginView } from './Login/LoginView.js';
 import Dropdown from '../src/components/Messages/Messages.js';
 import NavBar from './components/NavBar.js';
 import Dashboard from './components/Dashboard/Dashboard.jsx';
 import ProfilePage from './components/ProfilePage/ProfilePage.js';
 import FindTeammates from './components/FindTeammates/FindTeammates.jsx';
+import axios from 'axios';
 
 export default function App() {
   //const divRef = useRef(true);
   const [userId, setUserId] = useState(true);
-  const [page, setPage] = useState('login');
+  const [page, setPage] = useState(null);
+
+  const Axios = axios.create({
+    baseURL: 'http://localhost:3001',
+  });
+
+  useEffect(() => {
+    Axios
+    .get('/auth', {withCredentials: true})
+    .then((res) => {
+      console.log(res);
+      if (res.data.id !== null) {
+        setUserId(res.data.id);
+        setPage('games')
+      }
+    })
+    .catch(() => setPage('login'));
+  }, [Axios])
 
   return (
     <div className='App'>
