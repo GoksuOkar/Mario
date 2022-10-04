6d40052293ff192ee9360f7d33352d33d7f82197
Lynn <griff.lynn8@gmail.com>
2022-10-04 11:04:41 -0700

    fleshing out Comments component. Still need to figure out how to get username (possibly from session?)

--- client/src/components/GamePage/Comments.js
+++ client/src/components/GamePage/Comments.js
@@ -1,10 +1,18 @@
-
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const Comments = ({ list }) => {
   //get comments
   // display comments
   // add comment form
     //on submit, adds the comment to the list of comments in db.
+
+    const [comment, setComment] = useState({
+      // *** get username from session? ask about this *** \\
+      userName: '',
+      body: '',
+      date: new Date()
+    })
 
   useEffect(() => {
     //reload widget when a new comment is added to the list
@@ -21,8 +29,17 @@ const Comments = ({ list }) => {
   }
 
   const handleComSubmit = () => {
-    // when submitted, this should add the comment to the list of comments in db.
-      // it needs to get the current user's name.
+      axios.post('/events/comments', {
+        userName: comment.userName,
+        body: comment.body,
+        date: comment.date
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
 
@@ -32,7 +49,11 @@ const Comments = ({ list }) => {
         <form>
           <label>
             add a comment:
-            <input type="text" name="comment" />
+            <input
+              type="text"
+              name="comment"
+              value={comment.body}
+              onChange={e => setComment(prev => prev.body = e.target.value)}/>
           </label>
           <input type="submit" value="Submit" onSubmit={handleComSubmit}/>
         </form>
