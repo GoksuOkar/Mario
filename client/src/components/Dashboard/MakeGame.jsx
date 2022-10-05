e7cf227b874bde5a923ba7e04063d0858f010251
Thach Do <thachdo90@gmail.com>
2022-10-05 08:47:01 -0700

    gave ids to form elements

--- client/src/components/Dashboard/MakeGame.jsx
+++ client/src/components/Dashboard/MakeGame.jsx
@@ -1,20 +1,31 @@
+import { useState } from 'react';
 import './MakeGame.css';
 import StartGameImg from '../../assets/images/StartGameImg.png';
 import { TextInput, Textarea, Button } from '@mantine/core';
 import { TimeRangeInput, DatePicker } from '@mantine/dates';
 import 'dayjs/locale/en';
 
+// add animation for forms to slide out on closing
 const MakeGame = ({ setFormOpen }) => {
+  const [slideout, setSlideout] = useState(false);
 
   const handleSubmit = () => {
     // TODO
     // extract, validate, formate, send form
+    let body = {
+      eventName: document.getElementById('game-name'),
+      location: document.getElementById('location'),
+      date: document.getElementById('date'),
+      time: document.getElementById('time-range')
+    }
     setFormOpen(false);
 
   }
   return (
     <>
-      <div id='left-form-panel'>
+      <div id='left-form-panel'
+        onClick={()=>setFormOpen(false)}
+        >
         <img src={StartGameImg} alt='basketball img with text start game'></img>
       </div>
       <div id='right-form-panel'>
@@ -22,23 +33,28 @@ const MakeGame = ({ setFormOpen }) => {
           <TextInput
             placeholder='name of the game'
             label='Game Name:'
+            id='game-name'
             // withAsterisk
           />
           {/* can we integrate google maps here? */}
           <TextInput
             placeholder='address'
             label='Location:'
+            id='location'
           />
           <DatePicker
             placeholder="Pick a date"
             label="Date"
+            id='date'
           />
           <TimeRangeInput
             label='Start-End Time'
+            id='time-range'
           />
           <Textarea
             placeholder='add some description'
             label='Description'
+            id='game-description'
           />
           <Button
           //later: change color
