032f5c68d24c7211ea00684c2cf45874ec7f15a9
Blake Lenhard <lenhardbc@gmail.com>
2022-10-05 11:28:42 -0700

    created map component and copy config file

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { LoginView } from './Login/LoginView.js';
-import Dropdown from '../src/components/Messages/Messages.js';
-import NavBar from './components/NavBar.js';
-import Dashboard from './components/Dashboard/Dashboard.jsx';
-import ProfilePage from './components/ProfilePage/ProfilePage.js';
-import FindTeammates from './components/FindTeammates/FindTeammates.jsx';
+import React, { useState, useEffect } from "react";
+import { LoginView } from "./Login/LoginView.js";
+import Dropdown from "../src/components/Messages/Messages.js";
+import NavBar from "./components/NavBar.js";
+import Dashboard from "./components/Dashboard/Dashboard.jsx";
+import ProfilePage from "./components/ProfilePage/ProfilePage.js";
+import FindTeammates from "./components/FindTeammates/FindTeammates.jsx";
 // eslint-disable-next-line
-import axios from 'axios';
+import axios from "axios";
+import { Map } from "./components/GamePage/Map.js";
 
 export default function App() {
   //const divRef = useRef(true);
-  const [userId, setUserId] = useState('633ca1f73a3cb5d9bdc3bff5');
+  const [userId, setUserId] = useState("633ca1f73a3cb5d9bdc3bff5");
   const [page, setPage] = useState(null);
 
   const Axios = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: "http://localhost:3001",
   });
 
   // checks if the user is already authenticated, sets the page to 'login' if not.
   useEffect(() => {
-    Axios.get('/auth', { withCredentials: true })
+    Axios.get("/auth", { withCredentials: true })
       .then((res) => {
         console.log(res);
         if (res.data.id !== null) {
           setUserId(res.data.id);
-          setPage('games');
+          setPage("games");
         }
       })
-      .catch(() => setPage('login'));
+      .catch(() => setPage("login"));
   }, []);
 
   return (
-    <div className='App'>
-      <NavBar userId={userId} page={page} setPage={setPage} />
+    <div className="App">
+      {/* <NavBar userId={userId} page={page} setPage={setPage} />
       {page === 'login' ? (
         <LoginView setPage={setPage} setUserId={setUserId} userId={userId} />
       ) : null}
@@ -41,7 +42,8 @@ export default function App() {
       {page === 'profile' || page === 'frnd' ? (
         <ProfilePage userId={userId} page={page} setPage={setPage} />
       ) : null}
-      {page === 'findTeam' ? <FindTeammates /> : null}
+      {page === 'findTeam' ? <FindTeammates /> : null} */}
+      <Map />
     </div>
   );
 }
---
+++ client/src/components/GamePage/Map.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { API_KEY } from "../../config";
+
+export const Map = ({ address }) => {
+  //get address in correct format "City Hall, New York, NY" to City+Hall,New+York,NY,
+  address = address || "5020 Mindora Dr, Torrance, Ca, United States";
+  address = address.split(",");
+  let paramAddress = [];
+  for (let i = 0; i < address.length; i++) {
+    let split = address[i].split(" ");
+    if (i > 0) {
+      split.splice(0, 1);
+    }
+    if (split.length > 1) {
+      split = [split.join("+")];
+    }
+    paramAddress.push(split);
+  }
+  paramAddress = paramAddress.join(",");
+  paramAddress = `q=${paramAddress}`;
+  const src = `https://www.google.com/maps/embed/v1/place?key=${API_KEY}&${paramAddress}`;
+
+  return (
+    <iframe
+      title="map"
+      width="450"
+      height="250"
+      frameborder="0"
+      style={{ border: 0 }}
+      referrerpolicy="no-referrer-when-downgrade"
+      src={src}
+      allowfullscreen
+    ></iframe>
+  );
+};
